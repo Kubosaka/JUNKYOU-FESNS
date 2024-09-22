987c41ffc991e4e2a22b69ca6897730987963ee4
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-09-22 17:35:14 +0900

    ポーリングテスト

--- next-prisma-postgresql/src/app/page.tsx
+++ next-prisma-postgresql/src/app/page.tsx
@@ -1,38 +1,15 @@
-import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import PostForm from "@/component/posts/PostForm";
+import PostList from "@/component/posts/PostList";
+import { Box } from "@mui/material";
+import { Suspense } from "react";
 
-export async function GET() {
-  try {
-    const posts = await prisma.posts.findMany({
-      orderBy: { created_at: "desc" },
-    });
-
-    return NextResponse.json(posts, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Failed to get post" }, { status: 500 });
-  }
-}
-
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-
-  try {
-    const post = await prisma.posts.create({
-      data: {
-        context: body.content,
-        user_id: 1,
-        area_id: 1,
-        created_at: new Date(),
-      },
-    });
-
-    return NextResponse.json(post, { status: 201 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Failed to create post" },
-      { status: 500 }
-    );
-  }
+export default async function Post() {
+  return (
+    <Box>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostForm />
+        <PostList />
+      </Suspense>
+    </Box>
+  );
 }
