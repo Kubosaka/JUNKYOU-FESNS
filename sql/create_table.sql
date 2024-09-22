eaeffae93404eb17015fed66b0092854e1d2f51d
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-09-22 16:43:05 +0900

    sql追加

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,9 +1,19 @@
 drop table if exists users cascade;
+drop table if exists posts cascade;
 -- ----------------------------------------------------------------------------------------------------
 
 create table users (
     id serial not null,             -- ID
     name text not null,             -- ユーザー名
+    created_at timestamp not null,  -- 作成日時
+    primary key (id)
+);
+
+create table posts (
+    id serial not null,             -- ID
+    context text not null,          -- ユーザー名
+    user_id integer not null,       -- ユーザーID
+    area_id integer not null,       -- エリアID
     created_at timestamp not null,  -- 作成日時
     primary key (id)
 );
